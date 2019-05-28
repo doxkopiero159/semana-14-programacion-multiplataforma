0571a79eccaf046e09321b4f8306eb7ac42799f0
doxkopiero159 <piero.granda@tecsup.edu.pe>
2019-05-27 22:38:06 -0500

    first commit

---
+++ Ejercicios11/Ejercicios11/Ejercicios11.Android/TextToSpeech_Android.cs
@@ -0,0 +1,42 @@
+﻿
+
+using Android.Speech.Tts;
+using Ejercicios11.Droid;
+using Java.Lang;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(TextToSpeech_Android))]
+
+namespace Ejercicios11.Droid
+{
+    public class TextToSpeech_Android : Object, ITextToSpeech, TextToSpeech.IOnInitListener
+    {
+        TextToSpeech speaker;
+        string toSpeak;
+
+        public void Speak(string text)
+        {
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                toSpeak = text;
+                if (speaker == null)
+                    speaker = new TextToSpeech(MainActivity.Instance, this);
+                else
+                {
+                    speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+                }
+            }
+        }
+
+        #region IOnInitListener implementation
+        public void OnInit(OperationResult status)
+        {
+            if (status.Equals(OperationResult.Success))
+            {
+                speaker.Speak(toSpeak, QueueMode.Flush, null, null);
+            }
+        }
+        #endregion
+    }
+}
